0b407e6cef373e6332d7186b70d319e6e3eb64ba
maplerichie <10757316+maplerichie@users.noreply.github.com>
2023-03-26 22:06:52 +0800

    Fix

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,6 +1,6 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient } from "wagmi";
-import { optimismGoerli, optimism } from "wagmi/chains";
+import { optimismGoerli } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider, webSocketProvider } = configureChains(
